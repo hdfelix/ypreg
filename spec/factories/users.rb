aa279870cf239635d718bd0a01d3038f0d08c31c
Hector D. Felix <hdfelix@gmail.com>
2014-02-22 13:12:30 -0800

    Add initial User factory and spec file.

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+#spec/factories/users.rb
+require 'faker'
+FactoryGirl.define do
+	factory :user do
+		email {Faker::Internet::email}
+		password '123'
+		password_confirmation '123'
+	end
+end
+
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+#spec/models/user_spec.rb
+require 'spec_helper'
+
+describe User do
+	it "has a valid factory" do
+		build(:user).should be_valid
+	end
+	it "is invalid without an e-mail" do
+		build(:user, email: nil).should_not_be_valid
+	end
+end
