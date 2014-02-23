b0d0afbb80d408da2f096b5695a7cdb57c46ab36
Hector D. Felix <hdfelix@gmail.com>
2014-02-22 17:49:53 -0800

    Added initial user model and initial user test specs.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,19 @@ describe User do
 	it "has a valid factory" do
 		build(:user).should be_valid
 	end
+	it "is valid with a firstname, lastname, and email" do
+		user = User.new(
+			firstname: 'Hector',
+			lastname: 'Felix',
+			email: 'test@gmial.com')
+		expect(user).to be_valid
+	end
+
+	it "is invalid without a firstname"
+	it "is invalid without a lastname"
 	it "is invalid without an e-mail" do
 		build(:user, email: nil).should_not be_valid
 	end
+	it "is invalid with a duplicate email address"
+	it "returns an user's full name as a string"
 end
