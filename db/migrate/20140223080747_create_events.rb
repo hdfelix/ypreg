aee60c7e62f51242551b6b5713fdd893d6f745e6
Hector D. Felix <hdfelix@gmail.com>
2014-03-01 08:13:37 -0800

    Update create_event migration.

--- db/migrate/20140223080747_create_events.rb
+++ db/migrate/20140223080747_create_events.rb
@@ -2,8 +2,13 @@ class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
       t.string :title
+			t.integer :location_id
+			t.integer :event_type
 			t.datetime :begin_date
 			t.datetime :end_date
+			t.decimal	:registration_cost
+		  t.datetime :registration_open_date
+			t.datetime :registration_close_date
       t.timestamps
     end
   end
