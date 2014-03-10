8c48aac9a83b387b6702043a8cfd9d7b67364bd5
Hector D. Felix <hdfelix@gmail.com>
2014-03-10 18:15:55 -0400

    Add initial model specs for Address and Registration.

---
+++ spec/models/registration_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Registration do
+	it "has a valid factory" do
+	end
+
+	it "is invalid without a person_id" do
+	end
+
+	it "is invalid without a conference_id" do
+	end
+
+	it "is invalid without a registration_date" do
+	end
+	
+	it "is invalid without a payment type" do
+	end
+
+end
