316311f6f058d06629f4ad0dd56aa971380da0e6
Hector D. Felix <hdfelix@gmail.com>
2014-03-10 19:27:25 -0400

    Added more use of FactoryGirl, removed redundant test.

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,29 +1,30 @@
 require 'spec_helper'
 
 describe Location do
-	it "has a valid factory" do
+	it "has a valid factory" do #same as is valid with (required fields)
 		expect(build(:location)).to be_valid
 	end
 
-	it "is valid with name, description, and address_id" do
-		location = Location.new(
-			name: 'Test Location',
-			description: 'This location does not exist.',
-			address_id: '1')
-		expect(location).to be_valid
-	end
+	#it "is valid with name, description, and address_id" do
+	#	expect(build(:location)).to be_valid
+	#end
 
 	it "is invalid without a name" do
-		expect(Location.new(name: nil)).to have(1).errors_on(:name)	
+		expect(build(:location, name: nil)).to have(1).errors_on(:name)	
 	end
 
 	it "is invalid without a description" do
 		expect(Location.new(description: nil)).to have(1).errors_on(:description)
 	end
 	
-
 	it "is invalid without an address_id" do
 		expect(Location.new(address_id: nil)).to have(1).errors_on(:address_id)
 	end
-	
-end
+
+	it "does not allow two localities with the same address" do
+		create(:location, address_id: '1')
+		loc = build(:location, address_id: '1')
+
+	expect(loc).to have(1).errors_on(:address_id)
+	end
+end	
