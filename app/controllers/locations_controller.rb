bef592c3adb883bfdb267d3cc0e51efe446bf6e7
Hector D. Felix <hdfelix@gmail.com>
2014-03-16 20:39:27 -0400

    Fixed error after commenting out content for all methods generated via scaffold.

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,71 +4,71 @@ class LocationsController < ApplicationController
   # GET /locations
   # GET /locations.json
   def index
-#    @locations = Location.all
+    #@locations = Location.all
   end
 
-#  # GET /locations/1
-#  # GET /locations/1.json
+  # GET /locations/1
+  # GET /locations/1.json
   def show
   end
 
-#  # GET /locations/new
+  # GET /locations/new
   def new
-#    @location = Location.new
+    #@location = Location.new
   end
 
-#  # GET /locations/1/edit
+  # GET /locations/1/edit
   def edit
   end
 
-#  # POST /locations
-#  # POST /locations.json
+  # POST /locations
+  # POST /locations.json
   def create
-#    @location = Location.new(location_params)
-#
-#    respond_to do |format|
-#      if @location.save
-#        format.html { redirect_to @location, notice: 'Location was successfully created.' }
-#        format.json { render action: 'show', status: :created, location: @location }
-#      else
-#        format.html { render action: 'new' }
-#        format.json { render json: @location.errors, status: :unprocessable_entity }
-#      end
-#    end
+   # @location = Location.new(location_params)
+
+   # respond_to do |format|
+   #   if @location.save
+   #     format.html { redirect_to @location, notice: 'Location was successfully created.' }
+   #     format.json { render action: 'show', status: :created, location: @location }
+   #   else
+   #     format.html { render action: 'new' }
+   #     format.json { render json: @location.errors, status: :unprocessable_entity }
+   #   end
+   # end
   end
 
-#  # PATCH/PUT /locations/1
-#  # PATCH/PUT /locations/1.json
+  # PATCH/PUT /locations/1
+  # PATCH/PUT /locations/1.json
   def update
-#    respond_to do |format|
-#      if @location.update(location_params)
-#        format.html { redirect_to @location, notice: 'Location was successfully updated.' }
-#        format.json { head :no_content }
-#      else
-#        format.html { render action: 'edit' }
-#        format.json { render json: @location.errors, status: :unprocessable_entity }
-#      end
-#    end
+   # respond_to do |format|
+   #   if @location.update(location_params)
+   #     format.html { redirect_to @location, notice: 'Location was successfully updated.' }
+   #     format.json { head :no_content }
+   #   else
+   #     format.html { render action: 'edit' }
+   #     format.json { render json: @location.errors, status: :unprocessable_entity }
+   #   end
+   # end
   end
 
-#  # DELETE /locations/1
-#  # DELETE /locations/1.json
+  # DELETE /locations/1
+  # DELETE /locations/1.json
   def destroy
-#    @location.destroy
-#    respond_to do |format|
-#      format.html { redirect_to locations_url }
-#      format.json { head :no_content }
-#    end
-	 end
-#
+   # @location.destroy
+   # respond_to do |format|
+   #   format.html { redirect_to locations_url }
+   #   format.json { head :no_content }
+   # end
+  end
+
   private
-#    # Use callbacks to share common setup or constraints between actions.
+    # Use callbacks to share common setup or constraints between actions.
     def set_location
-#      @location = Location.find(params[:id])
+      #@location = Location.find(params[:id])
     end
-#
-#    # Never trust parameters from the scary internet, only allow the white list through.
+
+    # Never trust parameters from the scary internet, only allow the white list through.
     def location_params
-#      params.require(:location).permit(:name)
+      #params.require(:location).permit(:name)
     end
 end
