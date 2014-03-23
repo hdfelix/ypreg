230a4e9aa5b4353f06c167898fd556856c5c7612
Hector D. Felix <hdfelix@gmail.com>
2014-03-22 22:25:09 -0700

    Add Address model, factory, migration file.

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,6 +1,15 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :address do
+	sequence(:addressline1, 100) {|a| "#{n} Street #{a-95}" }
+	sequence(:city) {|c| "City#{n}" }
+	sequence(:zipcode, 92610) {|z| "#{n}" }
+	#sequence(:state_id, 1..49)
+  
+	factory :address do
+		addressline1
+		city
+		state_id '1'
+		zipcode
   end
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Address do
-  pending "add some examples to (or delete) #{__FILE__}"
+	it "has a valid factory"
+
+	it "is valid with addressline1, city, state"
+
+	it "is invalid without addressline1"
+
+	it "is invalid without a city"
+
+	it "is invalid without a state"
+
+	it "is invalid without a zipcode"
+	
 end
