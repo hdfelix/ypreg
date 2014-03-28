733721916037bd26195871d4c070e73d1e3a0958
Hector D. Felix <hdfelix@gmail.com>
2014-03-27 22:48:29 -0700

    Added i18n lines to suppress 'Deprecated' warning.

--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,9 @@ module YpwReg
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-		
+     config.i18n.default_locale = :en
+	  I18n.enforce_available_locales = true                                 
+
 		config.generators do |g|
 			g.stylesheets false
 			g.javascripts false
