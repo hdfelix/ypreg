0bd7cbba4bec0f79af64716fbb230f683254129c
Hector D. Felix <hdfelix@gmail.com>
2014-04-12 23:41:55 -0700

    Implemented Devise; added Welcome controller.

---
+++ db/migrate/20140413052850_drop_users.rb
@@ -0,0 +1,4 @@
+class DropUsers < ActiveRecord::Migration
+  def change
+  end
+end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,6 @@
-#spec/factories/users.rb
-require 'faker'
-FactoryGirl.define do
-	factory :user do
-		firstname {Faker::Name.first_name}		
-		lastname {Faker::Name.last_name}
-		email {Faker::Internet.email}
-		#password '123'
-		#password_confirmation '123'
-	end
-end
+# Read about factories at https://github.com/thoughtbot/factory_girl
 
+FactoryGirl.define do
+  factory :user do
+  end
+end
