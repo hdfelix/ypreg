56cc5652cecc10385dd55e7c6afe0441cebc2657
Hector D. Felix <hdfelix@gmail.com>
2014-04-25 23:21:04 -0700

    bug fixes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,48 +1,36 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :set_event, only: [ :show, :edit, :update, :destroy]
 
-  # GET /events
-  # GET /events.json
   def index
     @events = Event.all
   end
 
-  # GET /events/1
-  # GET /events/1.json
   def show
 		@post = Event.find(params[:id])
   end
 
-  # GET /events/new
   def new
     @event = Event.new
 		@event.build_location
 		puts "******* #{@event.location.to_yaml}"
   end
 
-  # GET /events/1/edit
   def edit
 		@post = Event.find(params[:id])
   end
 
-  # POST /events
-  # POST /events.json
   def create
-    #@event = Event.new(event_params)
-
-    #respond_to do |format|
-    #  if @event.save
-    #    format.html { redirect_to @event, notice: 'Event was successfully created.' }
-    #    format.json { render action: 'show', status: :created, location: @event }
-    #  else
-    #    format.html { render action: 'new' }
-    #    format.json { render json: @event.errors, status: :unprocessable_entity }
-    #  end
-    #end
+		@event = Event.new(event_params)
+		#raise
+		if @event.save
+			redirect_to event_path, notice: 'Event was successfully created.'
+		else
+			#raise
+			flash[:error] = "Error creating the event."
+			render action: 'new'
+		end
   end
 
-  # PATCH/PUT /events/1
-  # PATCH/PUT /events/1.json
   def update
     #respond_to do |format|
     #  if @event.update(event_params)
@@ -68,11 +56,12 @@ class EventsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_event
-    #  @event = Event.find(params[:id])
+      @event = Event.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-			#params.require(:event).permit(:title)
+			params.require(:event).permit(:title, 
+				locations_attributes: [:address1, :address2, :city, :state_abbrv, :zipcode])
     end
 end
