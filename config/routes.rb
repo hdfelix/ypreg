9915bc7ad00a3969f90bd9762aafbc40bddbc8cc
Hector D. Felix <hdfelix@gmail.com>
2014-05-04 12:50:24 -0700

    Added Registartion controller with index; Started registration form partial; needs form controls added registrations spec feature file with initial tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 YpwReg::Application.routes.draw do
-  
+
+  get "welcome/index"
   devise_for :users
   get "home/index"
 
 	resources :locations
 	resources :events
-	resources :registrations, only: [:index]
-
-	get "welcome/index"
+	resources :registrations, only: [:index, :new, :create]
 
   #get "login/index"
   # See how all your routes lay out with "rake routes".
---
+++ spec/features/user_manages_registration_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+feature 'User registers for an event' do
+	scenario ' - can see a list of up-coming events in Welcome page' do
+		@event = create(:event)
+
+		visit root_path
+		expect(page).to have_content ('Upcoming events')
+		expect(page).to have_content (@event.title)
+		expect(page).to have_content(@event.begin_date)
+		expect(page).to have_content(@event.end_date)
+		expect(page).to have_content('Register')
+	end
+
+	scenario '- can access new registration form' do
+		visit root_path
+		first(:link, 'Register').click
+		expect(page).to have_content('New Registration')
+	end
+end
+
+feature 'User creates registration successfully' do
+	scenario ' - with required fields'
+	scenario ' - with all fields'
+end
+
+feature 'User is unsuccessful in creating a registration' do
+	scenario ' - sees error messages'
+end
