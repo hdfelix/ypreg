0a6145f85d168d6a8a156d051ed84ac58b5f5584
Hector D. Felix <hdfelix@gmail.com>
2014-05-17 11:36:18 -0700

    update locations factory; add integration tests for registrations

--- spec/features/user_manages_registration_spec.rb
+++ spec/features/user_manages_registration_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-feature 'User registers for an event' do
+feature 'Signed-out user views events' do
 	scenario ' - can see a list of up-coming events in Welcome page' do
 		@event = create(:event)
 
@@ -9,18 +9,41 @@ feature 'User registers for an event' do
 		expect(page).to have_content (@event.title)
 		expect(page).to have_content(format_date(@event.begin_date))
 		expect(page).to have_content(format_date(@event.end_date))
-		expect(page).to have_content('Register')
 	end
 
-	scenario '- can access new registration form' do
+	scenario ' - before signing in sees \'sign in to register\' by events' do
 		visit root_path
-		first(:link, 'Register').click
-		expect(page).to have_content('New Registration')
+		expect(page).to have_content('Sign in to register')
+		expect(page).to not_have_content('Register')
 	end
 end
 
-feature 'User creates registration successfully' do
-	scenario ' - with required fields'
+feature 'Signed-in user' do
+	let (:authed_user) {
+		create_logged_in_user
+	}
+	@event = FactoryGirl.create(:event)
+
+	scenario '- can register for an event' do
+		visit root_path(authed_user)
+
+		expect(page).to have_content('Register')
+	end
+
+	#For now, client wants all payments to be done through locality
+	scenario ' - registers successfully with required fields' do
+
+		visit root_path(authed_user)
+		first(:link, 'Register').click
+		@registration = build(:registration)
+		expect {
+			#fill_in 'registration_date', with: @registration.registration_date
+			#fill_in 'has_been_paid', with: false
+			#find("#registration_payment_type").find(:xpath, "option[@value=\'#{@registration.payment_type}\']").select_option	
+		}.to change(Registration, :count).by(1)
+	end
+
+	#Create this test once we allow for individual payments through the site
 	scenario ' - with all fields'
 end
 
