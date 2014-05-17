cdc2ccd4fced5dceedd1fdfbd862e840775ba1e9
Hector D. Felix <hdfelix@gmail.com>
2014-05-17 09:19:25 -0700

    clean up spec; add proper factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,12 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
+require 'faker'
 
 FactoryGirl.define do
-  factory :user do
-  end
+	factory :user do
+		name {Faker::Name.first_name}		
+		#lastname {Faker::Name.last_name}
+		email {Faker::Internet.email}
+		password 'secretpassword'
+		password_confirmation 'secretpassword'
+	end
 end
--- spec/features/user_manages_location_spec.rb
+++ spec/features/user_manages_location_spec.rb
@@ -23,18 +23,8 @@ feature 'User manages a location' do
 			fill_in 'location[address1]', with: @location.address1
 			fill_in 'location[city]', with: @location.city
 			find("#location_state_abbrv").find(:xpath, "option[@value=\'#{@location.state_abbrv}\']").select_option
-			#select("#[Location::STATE_LIST[#{@location.state_abbrv}]")
-			#find("#location_state_abbrv").find("option[index_of(#{@location.state_abbrvj)]").click
-			#find("#location_state_abbrv").find(:xpath, 'option[@value=\'CA\']').select_option
-			#page.select(@location_state_abbrv, from: 'location_state_abbrv')
-			#find('#location_state_abbrv').find(:xpath, "option[5]").click
-			#find('#location_state_abbrv').find(:xpath, "#{Location::STATE_LIST.find { |h| h.index(@location.state_abbrv)}.first}").click
-			#select_by_value('location_state_abbrv',@location.state_abbrv)
 			fill_in 'location[zipcode]', with: @location.zipcode
-			binding.pry
-			save_and_open_page
 			click_button 'Create Location'
-			#save_and_open_page    #MENTOR
 		}.to change(Location, :count).by(1)
 		expect(current_path).to eq events_path
 		expect(page).to have_content('Locaction was successfully created.')
@@ -46,7 +36,6 @@ feature 'User manages a location' do
 		visit edit_location_path(@loc)
 		fill_in 'location[name]', with: 'My location'
 		click_button 'Update Location'
-		#save_and_open_page
 	end
 end
 
