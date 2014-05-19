97f715953c6517516cb8899533d91f8f0ea62196
Hector D. Felix <hdfelix@gmail.com>
2014-05-18 22:39:37 -0700

    Adding initial registration to event code; need to limit to one registration per user per event.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,8 +4,25 @@ class RegistrationsController < ApplicationController
 
 	def new
 		@registration = Registration.new
+		@event = Event.find(params[:event_id])
 	end
 
 	def create
+		binding.pry
+		@registration = current_user.registrations.build(registration_params)
+		if @registration.save
+			flash[:notice] = "Registration created succesfully"
+			redirect_to root_path
+		else
+			flash[:error] = "Error creating registration"
+			render 'new'
+		end
+
+	end
+
+	private
+
+	def registration_params
+		params.require(:registration).permit(:event_id)
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
+	has_many :registrations
+
 	def role?(base_role3)
 		role == base_role.to_s
 	end
