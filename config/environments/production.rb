a3f19e8d29dc6e7c386d5eb35fa56e87e49b15be
Hector D. Felix <hdfelix@gmail.com>
2014-06-15 20:54:26 -0700

    Testing...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ YpwReg::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
 	 
   # Compress JavaScripts and CSS.
@@ -28,7 +28,7 @@ YpwReg::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -61,7 +61,6 @@ YpwReg::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
-	config.assets.precompile += %w( main.css )
 
 	# Add vendor path for Kingsboard theme
 #		config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
