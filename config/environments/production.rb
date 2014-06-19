14889f563b4735be05f6a8e14204d1c15851b1fe
Hector D. Felix <hdfelix@gmail.com>
2014-06-18 21:51:18 -0700

    Fixed typo in app name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,5 +83,5 @@ YpwReg::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-	config.action_mailer.default_url_options = { :host => 'ypwreg-herokuapp.com' }
+	config.action_mailer.default_url_options = { :host => 'ypreg-herokuapp.com' }
 end
