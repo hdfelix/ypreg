bf4d2b6b90d57b27243e2b695c7979f785a2f5f7
Hector D. Felix <hdfelix@gmail.com>
2014-07-10 09:14:20 -0700

    Update for multiple localities

--- app/models/event.rb
+++ app/models/event.rb
@@ -34,21 +34,21 @@ class Event < ActiveRecord::Base
 		Locality.all.each do |locality|
 			#Load stats for one locality
 			loc = locality.city
-			stats[loc]["grand_total"] = self.users.where("locality_id = ?",locality.id).count 
+			stats[loc]['grand_total'] = self.users.where('locality_id = ?',locality.id).count 
 			# Create methods for these values once user types and pmt handling is finalized
-			stats[loc]["total_yp"] = [83]
-			stats[loc]["total_serving_ones"] = [14]
-			stats[loc]["total_trainees"] = [11]
-			stats[loc]["total_helpers"] = [8]
-			stats[loc]["amount_due"] = stats[loc]["grand_total"] * self.registration_cost
-			stats[loc]["actual_grand_total"] = [92]
-			stats[loc]["actual_total_yp"] = [56]
-			stats[loc]["actual_total_serving_ones"] = [8]
-			stats[loc]["actual_total_trainees"] = [6]
-			stats[loc]["actual_total_helpers"] = [3]
-			stats[loc]["actual_amount_paid"] = 73 * self.registration_cost
-			stats[loc]["balance"] = stats[loc]["actual_amount_paid"] - stats[loc]["amount_due"]
+      stats[loc]['total_yp'] = [83]
+      stats[loc]['total_serving_ones'] = [14]
+			stats[loc]['total_trainees'] = [11]
+			stats[loc]['total_helpers'] = [8]
+			stats[loc]['amount_due'] = stats[loc]['grand_total'] * self.registration_cost
+			stats[loc]['actual_grand_total'] = [92]
+			stats[loc]['actual_total_yp'] = [56]
+			stats[loc]['actual_total_serving_ones'] = [8]
+			stats[loc]['actual_total_trainees'] = [6]
+			stats[loc]['actual_total_helpers'] = [3]
+			stats[loc]['actual_amount_paid'] = 73 * self.registration_cost
+			stats[loc]['balance'] = stats[loc]['actual_amount_paid'] - stats[loc]['amount_due']
 		end
-		stats
-	end
+    stats
+  end
 end
