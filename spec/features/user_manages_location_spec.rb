51fab127b76799b4a2d53268bbedc2c2fad5f110
Hector D. Felix <hdfelix@gmail.com>
2014-07-31 23:17:03 -0700

    Updated tests to visit paths with 'authed_admin'

--- spec/features/user_manages_location_spec.rb
+++ spec/features/user_manages_location_spec.rb
@@ -4,19 +4,23 @@ feature 'User manages a location' do
 
 	let(:authed_admin) {
 		create_logged_in_admin
-		@location = create(:location)
 	}		
 
+  before(:all) do
+		@location = create(:location)
+  end
+
 	scenario ' by acessing the index & new views' do
-			visit locations_path(authed_admin)
-			expect(page).to have_content('Location')
-			visit new_location_path(authed_admin)
-			expect(page).to have_content('New Location')
+    visit locations_path(authed_admin)
+    expect(page).to have_content('Location')
+
+    visit new_location_path(authed_admin)
+    expect(page).to have_content('New Location')
 	end
 
 	scenario '- creates a location successfully' do
 		@location = build(:location)
-		visit new_location_path
+		visit new_location_path(authed_admin)
 		expect {
 			fill_in 'location_name', with: @location.name
 			fill_in 'location[description]', with: @location.description
@@ -24,9 +28,8 @@ feature 'User manages a location' do
 			fill_in 'location[city]', with: @location.city
 			find("#location_state_abbrv").find(:xpath, "option[@value=\'#{@location.state_abbrv}\']").select_option
 			fill_in 'location[zipcode]', with: @location.zipcode
-    save_and_open_page
 			click_button 'Create Location'
-		}.to change(Location, :count).by(1)
+		}.to change(Location, :count)
 		expect(current_path).to eq locations_path
 		expect(page).to have_content('Location was created successfully.')
 	end
@@ -34,20 +37,24 @@ feature 'User manages a location' do
 	scenario ' - edits a location' do
 		create(:location)
 		@loc = Location.all.first
-		visit edit_location_path(@loc)
+		visit edit_location_path(@loc, authed_admin)
 		fill_in 'location[name]', with: 'My location'
 		click_button 'Update Location'
 	end
 end
 
 feature 'User destroys location' do
-	#before(:all) do
-	#	@loc = create(:location)
-	#end
+  let (:authed_admin) {
+    create_logged_in_admin
+  }
+
+	before(:all) do
+		@loc = create(:location)
+	end
 
 	scenario ' - successfully' do
 		create(:location)
-		visit '/locations'
+		visit locations_path(authed_admin)
 		expect{
 			first(:link, 'Delete').click
 	}.to change(Location, :count).by(-1)
