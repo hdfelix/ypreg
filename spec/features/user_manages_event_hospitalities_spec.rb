c26e407f6994b48065d261454695bf3d4934e3f1
Hector D. Felix <hdfelix@gmail.com>
2014-08-16 20:00:33 -0700

    Further updates to fix master after recent merges

--- spec/features/user_manages_event_hospitalities_spec.rb
+++ spec/features/user_manages_event_hospitalities_spec.rb
@@ -8,6 +8,13 @@ feature 'User adds an event hospitality location' do
 		@event = create(:event)
 	end
 
+  #scenario '- clicks on \'Add Event Hospitality\'' do
+  #  #visit hospitality_assign_path(authed_admin)
+  #  visit event_path(@event, authed_admin)
+  #  wait_for_ajax
+  #  expect(page).to have_content('Add')
+  #  #find('#available_hospitalities_id').find(:xpath, 'option[1]').select_option
+  #end
   scenario '- adds one event hospitality'
   scenario '- adds multiple event hospitalities'
 end
