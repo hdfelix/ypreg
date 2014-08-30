d8ffc3b4458aa0da3ea54a8b4ef70c5a76b12fbb
Hector D. Felix <hdfelix@gmail.com>
2014-08-30 11:34:55 -0700

    clean up seeds file per RuboCop suggestions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+# This file should contain all the record creation needed to seed the database
+# with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside
+# the db with db:setup).
 #
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
 require 'faker'
 
-#Create 3 locations
+# Create 3 locations
 print "Running seeds file...\n\n"
 print "Creating Locations...\n"
 3.times do
@@ -22,7 +25,7 @@ print "Creating Locations...\n"
   )
 end
 
-#Create 5 localities
+# Create 5 localities
 print "Creating Localities...\n"
 5.times do
   Locality.create(
@@ -31,7 +34,7 @@ print "Creating Localities...\n"
   )
 end
 
-#Create Test event
+# Create Test event
 print "Creating Events...\n"
 tmp_date = Time.now + rand(1..3).months
 
@@ -47,53 +50,53 @@ Event.create(
 )
 
 print "Creating Users...\n"
-#Create an admin user
-admin= User.new(
+# Create an admin user
+admin = User.new(
   name: 'Hector D. Felix',
-  email: 'hdfelix@gmail.com', 
+  email: 'hdfelix@gmail.com',
   password: 'chiracha',
   password_confirmation: 'chiracha')
 
-  admin.skip_confirmation!
-  admin.save
-  admin.update_attributes(role: 'admin')
-  admin.update_attributes(locality_id: Locality.all.sample.id)
+admin.skip_confirmation!
+admin.save
+admin.update_attributes(role: 'admin')
+admin.update_attributes(locality_id: Locality.all.sample.id)
 
-#create yp account
+# Create yp account
 yp = User.new(
   name: 'YP User',
-  email: 'yp_user@ypreg.com', 
+  email: 'yp_user@ypreg.com',
   password: 'chiracha',
   password_confirmation: 'chiracha')
 
-  yp.skip_confirmation!
-  yp.save
-  yp.update_attributes(role: 'yp')
-  yp.update_attributes(locality_id: Locality.all.sample.id)
+yp.skip_confirmation!
+yp.save
+yp.update_attributes(role: 'yp')
+yp.update_attributes(locality_id: Locality.all.sample.id)
 
 scyp = User.new(
   name: 'SCYP User',
-  email: 'scyp_user@ypreg.com', 
+  email: 'scyp_user@ypreg.com',
   password: 'chiracha',
   password_confirmation: 'chiracha')
 
-  scyp.skip_confirmation!
-  scyp.save
-  scyp.update_attributes(role: 'scyp')
-  scyp.update_attributes(locality_id: Locality.all.sample.id)
+scyp.skip_confirmation!
+scyp.save
+scyp.update_attributes(role: 'scyp')
+scyp.update_attributes(locality_id: Locality.all.sample.id)
 
-#Create lodgings
-#lodging_type: Lodging::LODGING_TYPE.sample
+# Create lodgings
+# lodging_type: Lodging::LODGING_TYPE.sample
 
 print "Creating Lodgings...\n"
-lodging= Lodging.new(
-          name: "Felixes",
+lodging = Lodging.new(
+          name: 'Felixes',
           description: "Hector & Angela Felix's home",
           address1: '60 Georgetown',
           city: 'Irvine',
           state_abbrv: 'CA',
           zipcode: '92612',
-          max_capacity: 5, 
+          max_capacity: 5,
           min_capacity: 2,
           contact_person: User.first,
           locality_id: admin.locality_id,
@@ -106,15 +109,15 @@ lodging.update_attributes(name: 'Lodging 1')
 lodging = FactoryGirl.create(:lodging)
 lodging.update_attributes(name: 'Lodging 2')
 
-#Create event registration
+# Create event registration
 print "Creating an event registration...\n"
 
 ev = Event.first
 reg = Registration.new(
-        payment_type: 1, 
+        payment_type: 1,
         has_been_paid: false,
         payment_adjustment: ev.registration_cost - 10,
         attend_as_serving_one: true,
-        user_id: User.first.id, 
+        user_id: User.first.id,
         event_id: ev.id)
 reg.save
