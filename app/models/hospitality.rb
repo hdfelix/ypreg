e0a88d35fa6cfc61589cc631a5c7e9a5fa851dc1
Hector D. Felix <hdfelix@gmail.com>
2014-09-02 20:09:02 -0700

    Law of demeter updates; fix typo.

--- app/models/hospitality.rb
+++ app/models/hospitality.rb
@@ -5,6 +5,10 @@ class Hospitality < ActiveRecord::Base
   has_many :hospitality_assignments
   has_many :registrations, -> { uniq }, through: :hospitality_assignments
 
+  delegate :name, :description, :address1, :address2, :city, :state_abbrv,
+           :zipcode, :lodging_type, :locality_id, :max_capacity, :min_capacity,
+           to: :lodging
+
   def lodging
     Lodging.find(lodging_id)
   end
--- app/models/registration.rb
+++ app/models/registration.rb
@@ -3,7 +3,9 @@ class Registration < ActiveRecord::Base
   belongs_to :user
   belongs_to :event
   has_many :hospitality_assignments
-  has_many :hrspitalities, -> { uniq }, through: :hospitality_assignments
+  has_many :hospitalities, -> { uniq }, through: :hospitality_assignments
 
+  delegate :name, :email, :cell_phone, :home_phone, :work_phone, :birthday, #...
+           to: :user
   PAYMENT_TYPE = [['Cash', 1], ['Check', 2]]
 end
