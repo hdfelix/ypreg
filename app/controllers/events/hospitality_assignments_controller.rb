0ade9f2c7c2b3cba7f99790a8addf8d6448fd2fd
Hector D. Felix <hdfelix@gmail.com>
2014-09-02 20:09:43 -0700

    Initial work for hospitality assignments index view.

--- app/controllers/events/hospitality_assignments_controller.rb
+++ app/controllers/events/hospitality_assignments_controller.rb
@@ -1,5 +1,6 @@
 class Events::HospitalityAssignmentsController < ApplicationController
   def index
-    
+    @event = Event.find(params[:event_id])
+    @stats =  @event.load_locality_summary
   end
 end
