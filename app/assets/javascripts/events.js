a2a6a366a4f0ba239ecafe70f342b46f2296c6c1
Hector D. Felix <hdfelix@gmail.com>
2014-10-04 01:11:44 -0700

    Added select options to drop downs; prepared forms for Ajax

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,9 +1,8 @@
 $( document ).ready(function() {
   // alert('in events.js');
   $('.hector').css('color', 'red');
-  console.log("In event!");
- $('#event_begin_date').datepicker(
-  {dateformat: 'yy-mm-dd'});
+//  $('#event_begin_date').datepicker(
+//  {dateformat: 'yy-mm-dd'});
 //	$("#event_end_date").datepicker(
 //		{dateformat: 'yy-mm-dd'});
 //	$("#event_registration_open_date").datepicker(
