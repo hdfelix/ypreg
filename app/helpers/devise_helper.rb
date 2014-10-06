4a45a25859fd66cff5f16f223abd1a0a951eb200
Hector D. Felix <hdfelix@gmail.com>
2014-10-06 14:01:15 -0700

    layout fix

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -5,10 +5,10 @@ module DeviseHelper
     messages = resource.errors.full_messages
       .map { |msg| content_tag(:li, msg) }.join
     html = <<-HTML
-    <div class="alert alert-error alert-block"> <button type="button"
-    class="close" data-dismiss="alert">x</button>
-      #{messages}
-    </div>
+<div class="alert alert-error alert-block"> <button type="button"
+class="close" data-dismiss="alert">x</button>
+    #{messages}
+</div>
     HTML
 
     html.html_safe
