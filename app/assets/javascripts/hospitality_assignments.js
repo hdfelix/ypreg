8f0d9091bf93f54e0938520c0339ea2f9e97ba1c
Hector D. Felix <hdfelix@gmail.com>
2014-10-06 14:01:08 -0700

    Testing JS for hospitality assignments

--- app/assets/javascripts/hospitality_assignments.js
+++ app/assets/javascripts/hospitality_assignments.js
@@ -1,3 +1,20 @@
 $(function() {  
   //alert('alert in hospitality_assignments.js firing...');
+  $("select").live("change", function(e) {
+    e.preventDefault();
+    $.ajax({
+      url: "/events/hospitality_assignments/" + $(this).val(), //I think, it could be something else
+      dataType: "json",
+      contentType: "application/json; charset=utf-8",
+      success: function(data) {
+        //binding.pry
+        //you're returning a JSON object, so just iterate through it and bind it to
+        //the value attributes of each field
+      },
+      error: function(xhr,exception,status) {
+        //console.log("In error clause")
+        //catch any errors here
+      }
+    });
+  }); 
 });
