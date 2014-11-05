8d19cd23a95f0b650c2c7f75d84389f3a12d348a
Hector D. Felix <hdfelix@gmail.com>
2014-11-05 09:18:17 -0800

    Updated seeds file to be more useful.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,19 @@
 
 require 'faker'
 
-# Create 3 locations
-print "Running seeds file...\n\n"
-print "Creating Locations...\n"
-3.times do
+start_time = Time.now
+current_users_total = User.all.count
+current_Localities_total = Locality.all.count
+current_Lodgings_total = Lodging.all.count
+current_events_total = Event.all.count
+current_locations_total = Location.all.count
+
+print "\n\nRunning seeds file...\n"
+
+# Create 5 locations
+count = 5
+print "\nCreating #{count} Locations: "
+count.times do
   Location.create(
     name: Faker::Name.name,
     description: Faker::Lorem.sentence,
@@ -21,25 +30,44 @@ print "Creating Locations...\n"
     city: Faker::Address.city,
     state_abbrv: Faker::Address.state_abbr,
     zipcode: Faker::Address.zip_code,
-    max_capacity: rand(200..2000).round(-1)
+    max_capacity: rand(50..200).round(-1)
   )
+  print '.'
 end
 
-# Create 5 localities
-print "Creating Localities...\n"
-5.times do
+# Create 10 localities
+count = 10
+print "\nCreating #{count} Localities: "
+count.times do
   Locality.create(
     city: Faker::Address.city,
     state_abbrv: Faker::Address.state_abbr
   )
+  print '.'
 end
 
-# Create Test event
-print "Creating Events...\n"
+# Create  3 Test events
+count = 3
+print "\nCreating #{count} Events: "
 tmp_date = Time.now + rand(1..3).months
 
 Event.create(
   title: Faker::Lorem.words(1).first.capitalize + ' ' + Faker::Lorem.words(1).first.capitalize,
+  event_type: rand(1..2),
+  begin_date: tmp_date,
+  end_date: (tmp_date + 3.days).strftime('%Y/%m/%d'),
+  registration_open_date: (tmp_date - 1.month).strftime('%Y/%m/%d'),
+  registration_close_date: (tmp_date - 1.month + 15.days).strftime('%Y/%m/%d'),
+  registration_cost: rand(10..100).round(-1),
+  location_id: rand(1..Location.all.count)
+)
+print '.'
+
+tmp_date = Time.now + rand(3..4).months
+
+Event.create(
+  title: Faker::Lorem.words(1).first.capitalize + ' ' +
+    Faker::Lorem.words(1).first.capitalize,
   event_type: rand(1..3),
   begin_date: tmp_date,
   end_date: (tmp_date + 3.days).strftime('%Y/%m/%d'),
@@ -48,9 +76,28 @@ Event.create(
   registration_cost: rand(10..100).round(-1),
   location_id: rand(1..Location.all.count)
 )
+print '.'
 
-print "Creating Users...\n"
+tmp_date = Time.now + rand(4..7).months
+
+Event.create(
+  title: Faker::Lorem.words(1).first.capitalize + ' ' + 
+    Faker::Lorem.words(1).first.capitalize,
+  event_type: rand(1..3),
+  begin_date: tmp_date,
+  end_date: (tmp_date + 3.days).strftime('%Y/%m/%d'),
+  registration_open_date: (tmp_date - 1.month).strftime('%Y/%m/%d'),
+  registration_close_date: (tmp_date - 1.month + 15.days).strftime('%Y/%m/%d'),
+  registration_cost: rand(10..100).round(-1),
+  location_id: rand(1..Location.all.count)
+)
+print '.'
+
+total_users = 0
+print "\nCreating Users..."
+
 # Create an admin user
+print "\n  Admins (1): "
 admin = User.new(
   name: 'Hector D. Felix',
   email: 'hdfelix@gmail.com',
@@ -58,37 +105,61 @@ admin = User.new(
   password_confirmation: 'chiracha')
 
 admin.skip_confirmation!
-admin.save
 admin.update_attributes(role: 'admin')
 admin.update_attributes(locality_id: Locality.all.sample.id)
+admin.save
 
-# Create yp account
-yp = User.new(
-  name: 'YP User',
-  email: 'yp_user@ypreg.com',
-  password: 'chiracha',
-  password_confirmation: 'chiracha')
+print "."
 
-yp.skip_confirmation!
-yp.save
-yp.update_attributes(role: 'yp')
-yp.update_attributes(locality_id: Locality.all.sample.id)
+total_users =+ 1
 
-scyp = User.new(
-  name: 'SCYP User',
-  email: 'scyp_user@ypreg.com',
-  password: 'chiracha',
-  password_confirmation: 'chiracha')
+# Create yp accounts
+count = 100
+print "\n  YP accounts (#{count}): "
 
-scyp.skip_confirmation!
-scyp.save
-scyp.update_attributes(role: 'scyp')
-scyp.update_attributes(locality_id: Locality.all.sample.id)
+for i in 1..(count + 1) do
+  yp = User.new(
+    name: "YP User#{i}",
+    email: "yp_user#{i}@ypreg.com",
+    password: 'chiracha',
+    password_confirmation: 'chiracha')
+
+  yp.skip_confirmation!
+  yp.update_attributes(role: 'yp')
+  yp.update_attributes(locality_id: Locality.all.sample.id)
+  yp.save
+  
+  print '.'
+end
+
+total_users =+ count
+
+# Create SCYP accounts
+count = 5
+print "\n  SCYP accounts(#{count}): "
+
+for i in 1..(count + 1) do
+  scyp = User.new(
+    name: "SCYP User#{i}",
+    email: "scyp_user#{i}@ypreg.com",
+    password: 'chiracha',
+    password_confirmation: 'chiracha')
+
+  scyp.skip_confirmation!
+  scyp.update_attributes(role: 'scyp')
+  scyp.update_attributes(locality_id: Locality.all.sample.id)
+  scyp.save
+
+  print '.'
+end
+
+total_users =+ count
 
 # Create lodgings
 # lodging_type: Lodging::LODGING_TYPE.sample
 
-print "Creating Lodgings...\n"
+count = 30
+print "\nCreating #{count} lodgings: "
 lodging = Lodging.new(
           name: 'Felixes',
           description: "Hector & Angela Felix's home",
@@ -99,25 +170,43 @@ lodging = Lodging.new(
           max_capacity: 5,
           min_capacity: 2,
           contact_person: User.first,
-          locality_id: admin.locality_id,
+          locality_id: Locality.all.sample.id,
           lodging_type: 1)
 lodging.save
+print '.'
 
-lodging = FactoryGirl.create(:lodging)
-lodging.update_attributes(name: 'Lodging 1')
+for i in 1..30 do  #not adding 1 to idx (added lodging above already)
+  user = User.all.sample
+  lodging = FactoryGirl.create(:lodging,
+    name: "Lodging #{i}",
+    contact_person: user,
+    locality_id: user.locality_id)
+  print '.'
+end
 
-lodging = FactoryGirl.create(:lodging)
-lodging.update_attributes(name: 'Lodging 2')
-
-# Create event registration
-print "Creating an event registration...\n"
+# Create event registrations
+count = rand(0..total_users)
+print "\nCreating #{count} event registrations: "
 
 ev = Event.first
-reg = Registration.new(
-        payment_type: 1,
-        has_been_paid: false,
-        payment_adjustment: ev.registration_cost - 10,
-        attend_as_serving_one: true,
-        user_id: User.first.id,
-        event_id: ev.id)
-reg.save
+for i in 1..(count + 1) do
+  reg = Registration.new(
+          payment_type: 1,
+          has_been_paid: [true, false].sample,
+          payment_adjustment: ev.registration_cost - rand(0..ev.registration_cost),
+          attend_as_serving_one: [true, false].sample,
+          user_id: User.all.sample.id,
+          event_id: ev.id)
+  reg.save
+  print '.'
+end
+
+puts "Summary"
+puts "---------------------------------"
+puts "Prior users count:      #{current_users_total}. Added #{User.all.count}."
+puts "Prior localities count: #{current_Localities_total}. Added #{Locality.all.count}."
+puts "Prior lodgings count:   #{current_Lodgings_total}. Added #{Lodging.all.count}."
+puts "Prior events count:     #{current_events_total}. Added #{Event.all.count}."
+puts "Prior locations count:  #{current_locations_total}. Added #{Location.all.count}."
+puts "---------------------------------"
+puts "\n\nFinished in #{Time.now - start_time} seconds...\n\n"
