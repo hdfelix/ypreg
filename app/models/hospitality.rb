77ab56ce7a03c608eaf6b8f94d5aedf0c2c789a4
Hector D. Felix <hdfelix@gmail.com>
2014-11-28 19:42:56 -0800

    Fixed association error, added messages

--- app/models/hospitality.rb
+++ app/models/hospitality.rb
@@ -1,15 +1,21 @@
 # Hospitality model - A lodging available for an event
 class Hospitality < ActiveRecord::Base
-  belongs_to :events
-  belongs_to :lodgings
+  belongs_to :event
+  belongs_to :lodging
+  belongs_to :locality # think about this more...
+  # has_one :location   # thinks bout this more...
   has_many :hospitality_assignments
   has_many :registrations, -> { uniq }, through: :hospitality_assignments
 
   delegate :name, :description, :address1, :address2, :city, :state_abbrv,
-           :zipcode, :lodging_type, :locality_id, :max_capacity, :min_capacity,
-           to: :lodging
+    :zipcode, :lodging_type, :locality_id, :max_capacity, :min_capacity,
+    to: :lodging
 
   def lodging
     Lodging.find(lodging_id)
+  end
+
+  def hosted_locality
+    Locality.find(locality_id)
   end
 end
