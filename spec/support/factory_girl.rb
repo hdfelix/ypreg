0c723e8cb3fee0cfc984a29ffb99c699a94d11df
Hector D. Felix <hdfelix@gmail.com>
2014-11-28 19:17:28 -0800

    Lint factories

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -2,9 +2,9 @@ RSpec.configure do |config|
 	# Save time when using  FactoryGirl Methods
 	config.include FactoryGirl::Syntax::Methods
 
-  #config.before(:suite) do
-  #  FactoryGirl.lint
-  #end
+  config.before(:suite) do
+    FactoryGirl.lint
+  end
 
 end
 
