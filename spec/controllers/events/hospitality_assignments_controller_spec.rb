cc666384258d462690f7e9ef7e5c064d8fc0d288
Hector D. Felix <hdfelix@gmail.com>
2014-11-28 19:23:02 -0800

    Add Event association, validation, scope tests; add test for '#remaining_spaces'

--- spec/controllers/events/hospitality_assignments_controller_spec.rb
+++ spec/controllers/events/hospitality_assignments_controller_spec.rb
@@ -4,13 +4,6 @@ describe Events::HospitalityAssignmentsController, :type => :controller do
   # let(:valid_attributes) { { "lodging_id" => "", "locality_id" => "" } }
 
   describe "POST assign_lodging_to_locality" do
-    it "assigns a lodging unit to a locality" do
-      @event = create(:event)
-      @lodging = create(:lodging)
-      @locality = create(:locality)
-
-      pending 'Write this test'
-      
-    end
+    it "assigns a lodging unit to a locality"
   end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,32 +1,56 @@
 require 'rails_helper'
 
 describe Event, :type => :model do
-	it "has a valid factory" do 
-    expect(build(:event)).to be_valid
-	end
 
-	it "is valid with all attributes" do
-		event = create(:event)
-		expect(event).to be_valid
-	end
+  describe Event, "Associations" do
+    it { should belong_to :location }
+    it { should have_many :registrations }
+    it { should have_many :users }
+    it { should have_many :localities }
+    it { should have_many :hospitalities }
+    # it { should have_many :lodgings }
+    it { should have_many :hospitality_assignments }
+  end
 
-	it "is invalid without a title" do
-    expect(build(:event, title:nil)).to_not be_valid
-	end
+  describe Event, "Validations" do
+    it { should validate_presence_of :event_type }
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :begin_date }
+    it { should validate_presence_of :end_date }
+    it { should validate_presence_of :registration_cost }
+    it { should validate_presence_of :location_id }
+    it { should have_constant :EVENT_TYPE }
+  end
 
-	# We may want to allow the creation of events w/o a begin date
-	#(so team can create the event before the date details are finalized)
-	it "is invalid without a begin_date" do
-    expect(build(:event, begin_date: nil)).to_not be_valid
-	end
-	
-	# We may want to allow the creation of events w/o a end  date
-	#(so team can create the event before the date details are finalized)
-	it "is invalid without an end_date" do
-    expect(build(:event, end_date: nil)).to_not be_valid
-	end
-	
-	it "is invalid without the cost of attendance" do
-    expect(build(:event, registration_cost: nil)).to_not be_valid
-	end
+  describe Event, "Scopes" do
+    describe "#remaining_spaces" do
+      it "returns the max capacity of spaces when there are no registrations" do 
+        event = build_stubbed(:event)
+
+        expect(event.remaining_spaces).to eq (event.location.max_capacity)
+      end
+
+      it "returns the remaining number of available registrations" do
+        loc = build_stubbed(:location, max_capacity: 20)
+        event = create(:event_with_registrations, registrations_count: 3, location: loc)
+
+        expect(event.remaining_spaces).to eq (17)
+      end
+    end
+  end
+
+  # describe "#participating_localities" do
+  #   event = create(:event_with_registrations)
+  # end
+
+  describe "#registered_saints_per_locality" do
+    # it "returns a hash with registered users per locality" do
+    #   localities = FactoryGirl.create_list(:locality, 3)
+    #   users = FactoryGirl.create_list(:user, 3, locality_id: localities[1].id)
+    #   users << FactoryGirl.create_list(:user, 2, locality_id: localities[2].id)
+    #   users << FactoryGirl.create_list(:user, 4, loclity_id: localities[4].id)
+
+    #   # expect  
+    # end
+  end
 end
