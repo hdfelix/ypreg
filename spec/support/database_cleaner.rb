e71099c58b90d9baafdb7325bee840d9fd060535
Hector D. Felix <hdfelix@gmail.com>
2014-12-07 18:52:41 -0800

    Cleaning up setup files

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -16,12 +16,12 @@ RSpec.configure do |config|
     DatabaseCleaner.strategy = :transaction
   end
 
-	config.before(:each, :js => true) do
-		DatabaseCleaner.strategy = :truncation
-	end
-
 	config.before(:each) do
 		DatabaseCleaner.start
+	end
+
+	config.before(:each, :js => true) do
+		DatabaseCleaner.strategy = :truncation
 	end
 
 	config.after(:each) do
