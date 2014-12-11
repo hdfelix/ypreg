a4fbf2693b17687d0a1e1569a6f27c0fce5bc129
Hector D. Felix <hdfelix@gmail.com>
2014-12-10 23:44:43 -0800

    Add feature test for adding lodgings; added min_capacity to New Lodging form; Swapped 'Submit' and 'Cancel' buttons (tab order)

--- spec/features/user_manages_lodging_spec.rb
+++ spec/features/user_manages_lodging_spec.rb
@@ -9,5 +9,41 @@ feature 'User manages lodgings' do
     @lodging = create(:lodging)
   end
 
-  scenario 'Creates a lodging'
+  scenario 'Can access lodging#index' do
+    visit lodgings_path(authed_admin)
+
+    expect(page).to have_content('LODGING')
+    expect(page).to have_content('list of available lodging locations for conferences and retreats')
+  end
+
+  scenario 'Can add a lodging successfully' do
+    not_a_contact_person = create(:user)
+
+    visit lodgings_path(authed_admin)
+
+    click_link 'New Lodging'
+    save_and_open_page
+
+    expect(page).to have_content('NEW LODGING')
+
+
+    fill_in 'lodging[name]', with: @lodging.name
+    select "#{ Lodging::LODGING_TYPE[@lodging.lodging_type.to_i] }", from: 'lodging[lodging_type]'
+    select "#{ @lodging.locality.city }", from: 'lodging[locality_id]'
+    select "#{ not_a_contact_person.name }", from: 'contact_person_id'
+    fill_in 'lodging[min_capacity]', with: @lodging.min_capacity
+    fill_in 'lodging[max_capacity]', with: @lodging.max_capacity
+    fill_in 'lodging[address1]', with: @lodging.address1
+    fill_in 'lodging[address2]', with: @lodging.address2
+    fill_in 'lodging[city]', with: @lodging.city
+    find('#lodging_state_abbrv').find(:xpath, "option[@value=\'#{ @lodging.state_abbrv }\']").select_option
+    # select "#{Location::STATE_LIST[@lodging.state_abbrv]}", from: 'lodging[lodging_state_abbrv]'
+    fill_in 'lodging[zipcode]', with: @lodging.zipcode
+
+    click_button 'Submit'
+   
+    expect(page).to have_content('Lodging was created successfully.')
+    expect(page).to have_content(@lodging.name)
+
+  end
 end
