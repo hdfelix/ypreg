a20c9de257cf27140724a5b308a7a4d47847cd87
Hector D. Felix <hdfelix@gmail.com>
2014-12-20 21:50:36 -0800

    Popoulate selected value properly

--- app/controllers/events/hospitality_locality_assignments_controller.rb
+++ app/controllers/events/hospitality_locality_assignments_controller.rb
@@ -1,6 +1,5 @@
 class Events::HospitalityLocalityAssignmentsController < ApplicationController
   def index
-    binding.pry
     @event = Event.find(params[:event_id])
     @stats =  @event.load_locality_summary
   end
@@ -8,13 +7,15 @@ class Events::HospitalityLocalityAssignmentsController < ApplicationController
   def assign
     @event = Event.find(params[:event_id])
     hospitality_ids = params[:hospitality_locality_ids].keys
-    # locality_ids = params[:hospitality_locality_ids].values
 
     hospitality_ids.each do |id|
-      binding.pry
       hosp = @event.hospitalities.find(id)
       loc = params[:hospitality_locality_ids][id.to_sym][0]
-      loc = loc.to_i unless (loc.nil? || loc.empty?)
+      if loc.empty?
+        loc = nil
+      else
+        loc = loc.to_i
+      end
       hosp.update_attributes(locality_id: loc)
     end
     @event.save
