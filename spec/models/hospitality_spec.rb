a9e92a54e80532b096c2bf943fd143116d97059a
Hector D. Felix <hdfelix@gmail.com>
2014-12-20 23:43:29 -0800

    Remove obsolete test

--- spec/models/hospitality_spec.rb
+++ spec/models/hospitality_spec.rb
@@ -9,24 +9,4 @@ describe Hospitality, type: :model do
     it { should have_many :hospitality_assignments }
     it { should have_many(:registrations).through(:hospitality_assignments) }
   end
-
-  describe 'Interface' do
-    describe '#lodging' do
-      it 'returns the lodging represented by the Hospitality instance' do
-        lodging = create(:lodging)
-        hospitality = create(:hospitality, lodging: lodging)
-
-        expect(hospitality.lodging).to eq(lodging)
-      end
-    end
-
-    describe '#hosted_locality' do
-      it 'returns the locality being hosted at the hospitality instance' do
-        locality = create(:locality)
-        hospitality = create(:hospitality, locality: locality)
-
-        expect(hospitality.hosted_locality).to eq(locality)
-      end
-    end
-  end
 end
