73df0a0dcfc52a8597c22460eee980fb9f7141b1
Hector D. Felix <hdfelix@gmail.com>
2014-12-21 17:48:25 -0800

    comment out code for now"

--- app/models/hospitality_assignment.rb
+++ app/models/hospitality_assignment.rb
@@ -1,22 +1,22 @@
 # A hospitality assigment for an event
 class HospitalityAssignment < ActiveRecord::Base
-  belongs_to :registration, inverse_of: :hospitality_assignments
-  belongs_to :hospitality, inverse_of: :hospitality_assignments
-  belongs_to :locality
+  # belongs_to :registration, inverse_of: :hospitality_assignments
+  # belongs_to :hospitality, inverse_of: :hospitality_assignments
+  # belongs_to :locality
 
-  def hospitality
-    Hospitality.find(hospitality_id)
-  end
+  # def hospitality
+  #   Hospitality.find(hospitality_id)
+  # end
 
-  def registration
-    Registration.find(registration_id)
-  end
+  # def registration
+  #   Registration.find(registration_id)
+  # end
 
-  def locality
-    Locality.find(locality_id)
-  end
+  # def locality
+  #   Locality.find(locality_id)
+  # end
 
-  def saint
-    User.find(registration.user_id)
-  end
+  # def saint
+  #   User.find(registration.user_id)
+  # end
 end
