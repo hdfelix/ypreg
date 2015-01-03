9af1329d6af5117a4228018a2167907c9f6c313c
Hector D. Felix <hdfelix@gmail.com>
2015-01-03 14:20:40 -0800

    Format Phone, address

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,12 +12,22 @@ module ApplicationHelper
     Lodging.where('id not in (?)', ev.hospitalities.pluck(:lodging_id))
   end
 
-  def full_address(addressable)
+  def full_address(addressable,*options_hash)
     address = addressable.address1 + ' '
     address += addressable.address2 unless addressable.address2.nil?
+    address += '<br />' if options_hash.first[:two_lines]
     address += addressable.city + ', ' +
-      addressable.state_abbrv + ' ' +
+      addressable.state_abbrv + '&nbsp;&nbsp;' +
       addressable.zipcode.to_s
-    address
+    address.html_safe
+  end
+
+  def format_phone_number(phone)
+    tmp = phone
+    if tmp.length == 7
+      tmp.insert(3,') ').insert(0,'(')
+    elsif tmp.length == 10
+      tmp.insert(6,'-').insert(3,') ').insert(0,'(')
+    end
   end
 end
