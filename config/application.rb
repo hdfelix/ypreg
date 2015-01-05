594f57aaed482651b3f49e486bd0056e51f55a0f
Hector D. Felix <hdfelix@gmail.com>
2015-01-04 17:13:11 -0800

    Fixing error on production

--- config/application.rb
+++ config/application.rb
@@ -58,13 +58,13 @@ module YpwReg
     end
 
     # http://stackoverflow.com/a/7412056
-    ActionDispatch::Callbacks.after do
-      # Reload the factories
-      return unless (Rails.env.development? || Rails.env.test?)
-
-      unless FactoryGirl.factories.blank? # first init will load factories, this should only run on subsequent reloads
-        FactoryGirl.factories.clear
-        FactoryGirl.find_definitions
+    if (Rails.env.development? || Rails.env.test?)
+      ActionDispatch::Callbacks.after do
+        # Reload the factories
+        unless FactoryGirl.factories.blank? # first init will load factories, this should only run on subsequent reloads
+          FactoryGirl.factories.clear
+          FactoryGirl.find_definitions
+        end
       end
     end
   end
