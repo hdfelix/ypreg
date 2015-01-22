50b3888121ac7bc2c882e5f02d19d4b4f53f2a08
Joe Woodward <j@joewoodward.me>
2015-01-22 10:58:25 +0000

    fix logged in menu, delete redundant files, reorganise stylesheets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,37 +1,11 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
 //= require jquery
 //= require jquery_ujs
 //= require jquery.ui.all
+//= require modernizr
 //= require_directory ./kingadmin-v1.2/.
 //= require bootstrap-sprockets
 //= require bootstrap-datepicker
-// the datepicker is initialized in app/assets/javascripts/events.js which was
-// probably imported with require_tree before. It looks like you've removed the
-// require tree import deliberately, if not you need to add the = after // to actually import it
 //= require events
 //= jquery.dataTables.bootstrap
+//= require_tree .
 //= require_self
-//= require modernizr
-// ------------------
-// Not required are below
-// if they should be required you need to add '=' after // like they are above
-// ------------------
-// require_tree .
-// require kingadmin-v1.2/bootstrap-tour.custom
-// require kingadmin-v1.2/king-table
-// require kingadmin-v1.2/king-elements
-// require kingadmin-v1.2/king-components
-// require kingadmin-v1.2/king-common
-// require kingadmin-v1.2/king-page
-// require kingadmin-v1.2/select2
-// require_self
