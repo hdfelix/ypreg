63d8f26b814a69672b8ce4ba83ac8029c6974c4c
Hector D. Felix <hdfelix@gmail.com>
2015-06-14 23:22:47 -0700

    Minor typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   has_many :events, through: :registrations
   belongs_to :locality
 
-  # validations
+  # Constants
   GENDER = %w(Brother Sister)
   USER_ROLE = %w(admin scyp ycat loc_contact hosp_contact trainee speaking_brother supporting_brother helper yp user guest)
 
