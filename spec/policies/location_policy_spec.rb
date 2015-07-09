e1f3a41e311fcbebd7285462d6ef219ac78a8831
Hector D. Felix <hdfelix@gmail.com>
2015-07-09 15:45:17 -0700

    Pundit helper; Empty Location spec for now - approach not working b/c of authentication error.

--- spec/policies/location_policy_spec.rb
+++ spec/policies/location_policy_spec.rb
@@ -4,14 +4,3 @@ require 'spec_helper'
 # "undefined method 'none?' for nil:NilClass
 # http://thunderboltlabs.com/blog/2013/03/27/testing-pundit-policies-with-rspec/
 
-# describe LocationPolicy do
-#   subject { LocationPolicy.new(user, location) }
-# 
-#   let(:location) { FactoryGirl.create(:location) }
-# 
-#   context 'for a visitor' do
-#     let(:user) { FactoryGirl.create(:user, role: 'admin') }
-# 
-#     it { should_not permit(:index) }
-#   end
-# end
