1a8abf7167ee4e140dd74ca8c8db7024049d31d7
Hector D. Felix <hdfelix@gmail.com>
2015-07-09 14:03:06 -0700

    Multiple enhancements to lodgings: * Display current contact person in ddl in Lodging#edit * Change 'Show' button for clickable Lodging name * Udpate table style for Lodging#index

--- spec/helpers/lodging_helper_spec.rb
+++ spec/helpers/lodging_helper_spec.rb
@@ -1,12 +1,26 @@
 require 'rails_helper'
 
 describe LodgingHelper do
+  let(:contact_person) { build_stubbed(:user, name: 'Hector') }
+  let(:lodging) { build_stubbed(:lodging, contact_person: contact_person) }
+            
   describe '#contact_person_name' do
-    it "returns a lodging contact person's" do
-      person = create(:user, name: 'Hector')
-      lodge = create(:lodging, contact_person: person)
+    it "returns a lodging contact person's name" do
+      expect(contact_person_name(lodging)).to eq('Hector')
+    end
+  end
 
-      expect(contact_person_name(lodge)).to eq('Hector')
+  describe '#filtered_contact_person_collection' do
+    it 'returns a collection of users not assigned as contact_people' \
+        'with the contact_person (if any) for the current lodging' do
+      # TODO: figure out how to use mocks with class doubles to test this method
+      # u2 = build_stubbed(:user, name: 'Angela')
+      # allow(User).to receive(:not_contact_persons).and_return(u2)
+
+      # expect(filtered_contact_person_collection(contact_person).map(&:name)).
+      #   to include [contact_person.name]
+      # expect(User).should_receive(:not_contact_persons)
+
     end
   end
 end
