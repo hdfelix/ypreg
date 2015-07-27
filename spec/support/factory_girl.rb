3b36f4824d7ca4c837f0033884f4c419fecdb41b
Hector D. Felix <hdfelix@gmail.com>
2015-07-26 19:29:31 -0700

    Cleaning up tests: * DatabaseCleaner call around Factories lint call * Remove now uneeded Event.delete_all calls * change before(:all) to before(:each) * Remove some uneeded comments * update to sign_in helper to use user double

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -3,7 +3,9 @@ RSpec.configure do |config|
 	config.include FactoryGirl::Syntax::Methods
 
   config.before(:suite) do
+    DatabaseCleaner.start
     FactoryGirl.lint
+    DatabaseCleaner.clean
   end
 
 end
