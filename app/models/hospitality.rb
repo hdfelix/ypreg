45fd734e6e250d5c2dfbdaf9e46adea347e3aae0
Hector D. Felix <hdfelix@gmail.com>
2015-08-04 23:58:31 -0700

    Law of demeter updates with delegates

--- app/models/hospitality.rb
+++ app/models/hospitality.rb
@@ -1,4 +1,3 @@
-# Hospitality model - A lodging available for an event
 class Hospitality < ActiveRecord::Base
   belongs_to :event, inverse_of: :hospitalities
   belongs_to :lodging
@@ -7,10 +6,12 @@ class Hospitality < ActiveRecord::Base
   has_many :registrations, -> { uniq }, through: :hospitality_registration_assignments
 
   delegate :name, :description, :address1, :address2, :city, :state_abbrv,
-    :zipcode, :lodging_type, :locality_id, :max_capacity, :min_capacity,
-    to: :lodging, prefix: :lodging
+           :zipcode, :lodging_type, :locality_id, :max_capacity, :min_capacity,
+           to: :lodging,
+           prefix: :lodging
 
   delegate :city, :state_abbrv, :contact, :lodging_contact,
-    to: :locality,
-    prefix: :locality
+           to: :locality,
+           prefix: :locality,
+           allow_nil: true
 end
