cf7b214794a2b1b313dc4064544f8768bc400be2
Hector D. Felix <hdfelix@gmail.com>
2015-08-06 18:07:09 -0700

    Removing incomplete test

--- spec/features/user_manages_attendance_spec.rb
+++ spec/features/user_manages_attendance_spec.rb
@@ -5,13 +5,5 @@ feature 'User manages attendance to event' do
     create_logged_in_admin
   }
 
-  scenario 'can see event current hospitalities when assigning new ones' do
-    event = create(:event_with_registrations)
-
-    visit event_path(event, authed_admin)
-    find('#manage').find(:xpath, "option[@value='Attendance']").select_option
-    save_and_open_page
-
-    # expect(page).to have_content(event.hospitalities.first.lodging_name)
-  end
+  scenario 'can see event current hospitalities when assigning new ones'
 end
