f799f23d6855a265a8787a0fcef2e95a75645ccb
Hector D. Felix <hdfelix@gmail.com>
2015-08-08 21:37:36 -0700

    Add EventLocality.registrations method

--- app/models/event_locality.rb
+++ app/models/event_locality.rb
@@ -3,7 +3,15 @@ class EventLocality < ActiveRecord::Base
   belongs_to :event
   belongs_to :locality
 
+	def self.registrations(event, locality)
+		Registration.where(event: event, locality: locality)
+	end
+
   def users
     User.where(locality: locality)
   end
+
+	def registrations
+		EventLocality.where(event: event, locality: locality)
+	end
 end
