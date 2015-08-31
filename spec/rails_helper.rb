de90aa3c2884d2949fdd8e494d6d7ff32bf398d7
Hector D. Felix <hdfelix@gmail.com>
2015-08-30 22:20:55 -0700

    Force new expect syntax; clean up commented out code.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,3 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
@@ -14,14 +13,15 @@ Capybara.javascript_driver = :webkit
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 # Checks for pending migrations before tests are run.
-# If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
 	config.include FeatureLoginMacros
 	config.include ApplicationHelper
   config.include WaitForAjax
-#  config.include Devise::TestHelpers, type: :controller
   config.extend ControllerMacros, type: :controller
   config.use_transactional_fixtures = false
 
@@ -31,7 +31,6 @@ RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = true
 
   config.infer_spec_type_from_file_location!
-
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
