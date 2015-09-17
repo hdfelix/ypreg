57fb7e476dad0adc2e0ca615a13275df88fb6e74
Hector D. Felix <hdfelix@gmail.com>
2015-09-16 22:22:01 -0700

    Fix handling of authorization for controller tests. Add missing Event controller tests.

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,78 +1,101 @@
 require 'rails_helper'
 
 describe EventsController, :type => :controller do
+  before(:example) do
+    sign_in_user(double('user', :role? => 'admin'))
+  end
+describe "GET index" do
+    it 'assigns all events and past_events' do
+      now = Time.zone.now
+      event_over = create(:event, begin_date: 1.month.ago, end_date: 1.month.ago + 2.days)
+      event_not_over= create(:event, begin_date: now + 8.days, end_date: now + 10.days)
 
-  describe "GET index" do
-    it 'assigns all events' do
-      Event.delete_all
-      e1 = create(:event)
-      e2 = create(:event)
+      get :index
 
-      get 'index'
+      expect(assigns(:events)).to match_array([event_not_over])
+      expect(assigns(:past_events)).to match_array([event_over])
+    end
 
-      expect(assigns(:events)).to match_array([e1, e2])
+    it "is a success" do
+      get :index
+      expect(response).to have_http_status('200')
+    end
+
+    it "renders 'index' template" do
+      get :index
+      expect(response).to render_template(:index)
     end
   end
 
-  #describe "GET show" do
-  #  it "assigns the requested event as @event" do
-  #    event = Event.create! valid_attributes
-  #    get :show, {:id => event.to_param}, valid_session
-  #    assigns(:event).should eq(event)
-  #  end
-  #end
+  describe "GET show" do
+    it "assigns the requested event to @event and \
+        localities participating to @participating_localities" do
+      event = create(:event_with_registrations)
+      participating_localities = event.participating_localities.sort { |a,b| a.city <=> b.city }
+      stats = event.load_locality_summary
 
-  #describe "GET new" do
-  #  it "assigns a new event as @event" do
-  #    get :new, {}, valid_session
-  #    assigns(:event).should be_a_new(Event)
-  #  end
-  #end
+      get :show, { id: event.to_param }
 
-  #describe "GET edit" do
-  #  it "assigns the requested event as @event" do
-  #    event = Event.create! valid_attributes
-  #    get :edit, {:id => event.to_param}, valid_session
-  #    assigns(:event).should eq(event)
-  #  end
-  #end
+      expect(assigns(:event)).to eq(event)
+      expect(assigns(:participating_localities)).to eq(participating_localities)
+      expect(assigns(:stats)).to eq(stats)
+    end
+  end
 
-  #describe "POST create" do
-  #  describe "with valid params" do
-  #    it "creates a new Event" do
-  #      expect {
-  #        post :create, {:event => valid_attributes}, valid_session
-  #      }.to change(Event, :count).by(1)
-  #    end
+  describe "GET new" do
+    it "assigns a new event as @event" do
+      get :new, {}
+      expect(assigns(:event)).to be_a_new(Event)
+    end
+  end
 
-  #    it "assigns a newly created event as @event" do
-  #      post :create, {:event => valid_attributes}, valid_session
-  #      assigns(:event).should be_a(Event)
-  #      assigns(:event).should be_persisted
-  #    end
+  describe "GET edit" do
+    it "assigns the requested event as @event" do
+      event = create(:event)
 
-  #    it "redirects to the created event" do
-  #      post :create, {:event => valid_attributes}, valid_session
-  #      response.should redirect_to(Event.last)
-  #    end
-  #  end
+      get :edit, { id: event.to_param }
 
-  #  describe "with invalid params" do
-  #    it "assigns a newly created but unsaved event as @event" do
-  #      # Trigger the behavior that occurs when invalid params are submitted
-  #      Event.any_instance.stub(:save).and_return(false)
-  #      post :create, {:event => { "title" => "invalid value" }}, valid_session
-  #      assigns(:event).should be_a_new(Event)
-  #    end
+      expect(assigns(:event)).to eq(event)
+    end
+  end
 
-  #    it "re-renders the 'new' template" do
-  #      # Trigger the behavior that occurs when invalid params are submitted
-  #      Event.any_instance.stub(:save).and_return(false)
-  #      post :create, {:event => { "title" => "invalid value" }}, valid_session
-  #      response.should render_template("new")
-  #    end
-  #  end
-  #end
+  describe "POST create" do
+    describe "with valid params" do
+      it "creates a new Event" do
+        expect {
+          post :create, event: attributes_for(:event).merge(location_id: 1)
+        }.to change(Event, :count).by(1)
+      end
+
+      it "assigns a newly created event as @event" do
+        post :create, event: attributes_for(:event).merge(location_id: 1)
+
+        expect(assigns(:event)).to be_an(Event)
+        expect(assigns(:event)).to be_persisted
+      end
+
+      it "redirects to the event index" do
+        post :create, event: attributes_for(:event).merge(location_id: 1)
+        expect(response).to redirect_to events_path
+      end
+    end
+
+    describe "with invalid params" do
+      it "assigns a newly created but unsaved event as @event" do
+        # Trigger the behavior that occurs when invalid params are submitted
+        Event.any_instance.stub(:save).and_return(false)
+        post :create, { event: { title: "" }}
+        expect(assigns(:event)).to be_a_new(Event)
+      end
+
+      it "re-renders the 'new' template" do
+        # Trigger the behavior that occurs when invalid params are submitted
+        Event.any_instance.stub(:save).and_return(false)
+        post :create, { event: { title: "" }}
+        expect(response).to render_template("new")
+      end
+    end
+  end
 
   #describe "PUT update" do
   #  describe "with valid params" do
