5f26c82a3b252c04e5ba80b2ad786a67614a4cd1
Hector D. Felix <hdfelix@gmail.com>
2015-09-20 00:25:57 -0700

    Update RSpec call

--- spec/controllers/lodgings_controller_spec.rb
+++ spec/controllers/lodgings_controller_spec.rb
@@ -100,7 +100,8 @@ describe LodgingsController, type: :controller do
       before(:example) do
         allow_any_instance_of(Lodging).to receive(:save).and_return(false)
         @user = build_stubbed(:user)
-        User.stub(:find).and_return(@user)
+        allow(User).to receive(:find).and_return(@user)
+        # User.stub(:find).and_return(@user)
       end
 
       it 'assigns a newly created but unsaved lodging as @lodging' do
