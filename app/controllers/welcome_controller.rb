6f42ab768699114f9b2d7d3ca400a305d7f03102
Hector D. Felix <hdfelix@gmail.com>
2015-09-21 11:43:12 -0700

    Remove dead code

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,5 @@
 class WelcomeController < ApplicationController
   def index
     @events = Event.in_the_future
-
-    # Values for aria chart
-    # @chart_values = widget_stats_next_event
   end
 end
