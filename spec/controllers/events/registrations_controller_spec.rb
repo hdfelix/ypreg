e046b94e3217b06e96c6b0233b57a5bde67bddb8
Philip Bradley <pdbradley@gmail.com>
2015-09-23 13:36:01 -0400

    fix registrations controller spec

--- spec/controllers/events/registrations_controller_spec.rb
+++ spec/controllers/events/registrations_controller_spec.rb
@@ -1,28 +1,27 @@
 require 'rails_helper'
 
-# describe Events::RegistrationsController, type: :controller do
-#   before(:example) do
-#     sign_in_user(double('user', role?:'admin'))
-#   end
-# 
-#   describe "GET #index" do 
-#     context "without params[:view] == 'attendance'" do
-#       it 'renders the :index view' do
-#         create(:event_with_registrations)
-#         get :index 
-#         expect(response).to render_template :index
-#       end
-#     end
-#   end
-#
-#    context "with params[:view] == 'attendance'" do
-#      it 'renders the :attendance_index view' do
-#        create(:event_with_registrations)
-#
-#        get 'index', view: 'attendance'
-#
-#        expect(response).to render_template :attendance_index
-#      end
-#    end
-#  end
-# end
+describe Events::RegistrationsController, type: :controller do
+  before(:example) do
+    sign_in_user(double('user', role?:'admin'))
+  end
+ 
+  describe "GET #index" do 
+    context "without params[:view] == 'attendance'" do
+      it 'renders the :index view' do
+        event = create(:event_with_registrations)
+        get :index, event_id: event.id
+        expect(response).to render_template :index
+      end
+    end
+
+    context "with params[:view] == 'attendance'" do
+      it 'renders the :attendance_index view' do
+        event = create(:event_with_registrations)
+
+        get 'index', event_id: event.id, view: 'attendance'
+
+        expect(response).to render_template :attendance_index
+      end
+    end
+  end
+end
