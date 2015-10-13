1f935ed6c8185a69729c55a0d6032bdca30f73c7
Hector D. Felix <hdfelix@gmail.com>
2015-10-13 13:45:21 -0700

    safety catch if running specs in production environment

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,9 @@ SimpleCov.start 'rails'
 
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'spec_helper'
 require 'rspec/rails'
 require 'shoulda/matchers'
 require 'capybara/rspec'
