a6c611f8ceae2ee9990534671f98af333cf37fed
Hector D. Felix <hdfelix@gmail.com>
2015-10-13 15:42:40 -0700

    Updates to LocationPolicy with tests.

--- spec/policies/location_policy_spec.rb
+++ spec/policies/location_policy_spec.rb
@@ -3,17 +3,24 @@ require 'rails_helper'
 describe LocationPolicy do
   subject { described_class }
 
-  let (:current_user) { FactoryGirl.build_stubbed :user }
-  let (:other_user) { FactoryGirl.build_stubbed :user }
   let (:admin) { FactoryGirl.build_stubbed :user, role: 'admin' }
+  let (:scyp_user) { FactoryGirl.build_stubbed :user, role: 'scyp' }
+  let (:other_user) { FactoryGirl.build_stubbed :user }
 
-  permissions :index?, :show?, :new?, :create?, :update?, :destroy? do
-    it "denies access if not an admin" do
-      expect(subject).not_to permit(current_user)
+  permissions :index?, :show?, :new?, :edit?, :create?, :update?, :destroy? do
+    context 'denies access to' do
+      it 'all but admin and scyp users' do
+        expect(subject).not_to permit(other_user)
+      end
     end
 
-    it "allows access for an admin" do
-      expect(subject).to permit(admin)
-    end 
+    context 'allows access to' do
+      it 'admins' do
+        expect(subject).to permit(admin)
+      end
+      it 'scyp users' do
+        expect(subject).to permit(scyp_user)
+      end
+    end
   end
 end
