64f1fcaff8aa7ff56199a29dd85331c143a78252
Hector D. Felix <hdfelix@gmail.com>
2015-10-13 15:05:47 -0700

    Add tests for EventPolicy

---
+++ spec/policies/event_policy_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe EventPolicy do
+  subject { described_class }
+
+  let (:current_user) { FactoryGirl.build_stubbed :user }
+  let (:other_user) { FactoryGirl.build_stubbed :user }
+  let (:admin) { FactoryGirl.build_stubbed :user, role: 'admin' }
+
+  permissions :index?, :new?, :create? do
+    it 'denies access if not an admin' do
+      expect(subject).not_to permit(current_user)
+    end
+
+    it 'allows access to admins' do
+      expect(subject).to permit(admin)
+    end
+  end
+end
