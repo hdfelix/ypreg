90ccc982a80c534c05864e7593d674be4dcca5dc
Hector D. Felix <hdfelix@gmail.com>
2015-10-16 14:14:58 -0700

    Refactor, cleanup code.

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,18 +1,12 @@
 module DashboardHelper
   def event_widget_header
-
-    if current_or_future_events_present?
-      content_tag('h5',
-                  content_tag(:i,'', class: 'fa fa-info-circle') +
-                  " #{shorten(Event.next.first.title, 0, 30, true)}",
-                  class: 'label label-default')
-    else
-      content_tag(:h5, 
-                  content_tag(:i,'', class: 'fa fa-info-circle'),
-                  class: 'label label-default'
-                 )
-    end
+    content = current_or_future_events_present? ? Event.next.first.title : ''
+    content_tag('h5',
+                content_tag(:i, '', class: 'fa fa-info-circle') +
+                " #{shorten(content, 0, 30, true)}",
+                class: 'label label-default')
   end
+
   def current_or_future_events_present?
     !Event.next.first.nil?
   end
@@ -26,25 +20,27 @@ module DashboardHelper
     if current_or_future_events_present?
       "#{chart_values[hash_id]}"
     else
-      "0 / 0"
+      '0 / 0'
     end
   end
 
-  def data_value_progress_div(chart_values)
-    content_tag(:div, aria_chart_percentage(chart_values), class: 'progress progress-xs') 
+  def data_value_progress_div(chart_values, attribute_name)
+    content_tag(:div,
+                aria_chart_percentage(chart_values, attribute_name),
+                class: 'progress progress-xs')
   end
 
-  def aria_chart_percentage(chart_values)
+  def aria_chart_percentage(chart_values, attribute_name)
     content_tag(:div,
                 aria_chart_percentage_span,
-                bar_chart_attributes(chart_values))
+                bar_chart_attributes(chart_values, attribute_name))
   end
 
   def aria_chart_percentage_span
     content_tag(:span, '10%', class: 'sr-only')
   end
 
-  def bar_chart_attributes(chart_values)
+  def bar_chart_attributes(chart_values, attribute_name)
     { class: 'progress-bar progress-bar-success',
       role: 'progressbar',
       aria: { valuenow: chart_values['att_value_now'],
