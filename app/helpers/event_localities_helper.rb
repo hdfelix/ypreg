b7ba9fac4a7ac292c17bed9d1c852e8cdc13e3a0
Hector D. Felix <hdfelix@gmail.com>
2015-10-19 23:50:15 -0700

    Change backgrond_check_expiration references; BCD updates.
    * Changed column sizes in Event/localities to accomidate for BCD
    * Add link to users from user names on 'Registered Saints' table
    * Set up code for User decorators  on this page
    * Class is only coloring on hover.... need to figure out why.

---
+++ app/helpers/event_localities_helper.rb
@@ -0,0 +1,11 @@
+module EventLocalitiesHelper  
+  def decorated_user(user)
+    # authorize users
+
+    if user.role == 'yp'
+      YpUserDecorator.decorate(user)
+    else
+      UserDecorator.decorate(user)
+    end
+  end
+end
