e6581d17b792d54469c94920c92d1942aed29881
Hector D. Felix <hdfelix@gmail.com>
2015-10-25 17:35:01 -0700

    Visual updates for expired background check
    Only display the warning icon for adults (18 or older) with
    expired background check.

--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -1,5 +1,13 @@
 module RegistrationsHelper
   def not_paid_in_words(has_been_paid)
-    "NOT PAID" unless has_been_paid == true
+    'NOT PAID' unless has_been_paid == true
+  end
+
+  def add_alert_style?(user)
+    if user.background_check_valid?
+      ''
+    else
+      "style='background-color: pink;'".html_safe
+    end
   end
 end
