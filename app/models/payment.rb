0ae46257ac10d66e3197c9698155bc9cb1a0b4ae
Hector D. Felix <hdfelix@gmail.com>
2015-11-28 16:29:04 -0800

    Fix failing test - Modify string conctactenation

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,8 +1,8 @@
 class Payment
   def self.tips
     { check_payment_instructions:
-      "Checks should be made out to:<br /><strong>Church in Anaheim -"\
-      "YP</strong><br /> and presented to the registration table"\
+      "Checks should be made out to:<br /><strong>Church in Anaheim - YP"\
+      "</strong><br /> and presented to the registration table"\
       "when arriving at the conference." }
   end
 end
