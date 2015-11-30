e663f544f3d2a0e531f5183ced871c7891b95608
Hector D. Felix <hdfelix@gmail.com>
2015-11-29 18:06:22 -0800

    Update Registraiton factory default value for payment_adjustment

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -195,7 +195,7 @@ FactoryGirl.define do
   factory :registration do
     payment_type 'cash'
     has_been_paid false
-    payment_adjustment '5'
+    payment_adjustment '0'
     attend_as_serving_one false
     hospitality nil
     locality { user.locality }
