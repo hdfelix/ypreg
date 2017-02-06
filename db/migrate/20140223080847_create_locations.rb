319e8c65b177ec12372a58ae84304158b2cf3b6e
Stephen Medina <stephen@lilmail.xyz>
2017-02-05 18:58:30 -0800

    fix seeding for locations

--- db/migrate/20140223080847_create_locations.rb
+++ db/migrate/20140223080847_create_locations.rb
@@ -2,13 +2,13 @@ class CreateLocations < ActiveRecord::Migration
   def change
     create_table :locations do |t|
       t.string :name
-			t.text :description
-			t.string :address1
-			t.string :address2
-			t.string :city
-			t.string :state_abbrv
-			t.integer :zipcode
-
+      t.text :description
+      t.string :address1
+      t.string :address2
+      t.string :city
+      t.string :state_abbrv
+      t.integer :zipcode
+      
       t.timestamps
     end
   end
