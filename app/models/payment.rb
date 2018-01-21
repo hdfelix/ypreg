2889ebc364fe124c490669b30094047e015cb3f1
Hector D. Felix <hdfelix@gmail.com>
2018-01-20 17:33:01 -0800

    Fix test

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,8 +1,15 @@
 class Payment
   def self.tips
-    { check_payment_instructions:
-      "Checks should be made out to:<br /><strong>Church in Anaheim - YP"\
-      "</strong><br /> and presented to the registration table"\
-      "when arriving at the conference." }
+    notice =
+      "Checks should be made out to:
+       <br />
+       <strong>Church in Anaheim - YP</strong>
+       <br />
+       <br />
+       <strong>2528 W. La Palma Ave.
+       <br />Anaheim, CA 92801</strong>
+       <br />
+       and presented to the registration table when arriving at the conference.".squish
+    { check_payment_instructions: notice }
   end
 end
--- spec/models/payment.rb
+++ spec/models/payment.rb
@@ -4,21 +4,18 @@ describe Payment, type: :model do
   describe 'Instance Methods' do
     describe '.tips' do
       it 'Returns an Hash of available system notes' do
-        expect(Payment.tips)
-          .to eq(
-            check_payment_instructions: "Checks should be made out to:
-                                         <br />
-                                         <strong>Church in Anaheim - YP</strong>
-                                         <br />
-                                         <br />
-                                         <strong>2528 W. La Palma Ave.
-                                         <br />Anaheim, CA 92801</strong>
-                                         <br />"
-          )
-      end
-
-      it 'failure' do
-        exect(true).to eq false
+        notice =
+          "Checks should be made out to:
+           <br />
+           <strong>Church in Anaheim - YP</strong>
+           <br />
+           <br />
+           <strong>2528 W. La Palma Ave.
+           <br />Anaheim, CA 92801</strong>
+           <br />
+           and presented to the registration table when arriving at the conference."
+          .squish
+        expect(Payment.tips).to eq(check_payment_instructions: notice)
       end
     end
   end
