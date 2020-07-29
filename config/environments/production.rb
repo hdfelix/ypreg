5d8f24e32de2f14fe94a440518c01424cff8ab3f
Hector D. Felix <hdfelix@gmail.com>
2020-07-28 17:14:02 -0700

    set config.assets.compile to false in staging / prod
    * ref: https://devcenter.heroku.com/articles/rails-asset-pipeline#compile-set-to-true-in-production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,8 @@ YpwReg::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false # TODO: HDF - Was true
+  # ref: https://devcenter.heroku.com/articles/rails-asset-pipeline#compile-set-to-true-in-production
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version`A have moved to config/initializers/assets.rb
 
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -27,7 +27,8 @@ YpwReg::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  # ref: https://devcenter.heroku.com/articles/rails-asset-pipeline#compile-set-to-true-in-production
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
